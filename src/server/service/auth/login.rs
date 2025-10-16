fc08d25bc6ca14b2fb5b793fb0ceb4249d8611bb
Hyziri <hyziri@protonmail.com>
2025-10-16 04:11:07 -0500

    tests: add unit tests for login service

--- src/server/service/auth/login.rs
+++ src/server/service/auth/login.rs
@@ -10,3 +10,41 @@ pub fn login_service(
 
     Ok(login)
 }
+
+#[cfg(test)]
+pub mod tests {
+    use crate::server::{
+        error::Error,
+        service::auth::login::login_service,
+        util::test::{test_setup, USER_AGENT},
+    };
+
+    /// Test successful login
+    #[tokio::test]
+    async fn test_login_service() {
+        let test = test_setup().await;
+
+        let scopes = vec![];
+        let result = login_service(&test.state.esi_client, scopes);
+
+        assert!(result.is_ok())
+    }
+
+    /// Test server error when OAuth2 for ESI client is not configured
+    #[test]
+    fn test_login_server_error() {
+        let esi_client = eve_esi::Client::new(USER_AGENT).expect("Failed to build ESI client");
+
+        let scopes = vec![];
+        let result = login_service(&esi_client, scopes);
+
+        assert!(result.is_err());
+
+        assert!(matches!(
+            result,
+            Err(Error::EsiError(eve_esi::Error::OAuthError(
+                eve_esi::OAuthError::OAuth2NotConfigured
+            )))
+        ))
+    }
+}
