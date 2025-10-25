be196eff4080e534361e9b45dc6eece7f745ed02
Hyziri <hyziri@protonmail.com>
2025-10-25 02:47:59 -0500

    feat: get or create user & character ownership entry during callback which now returns user ID

--- src/model/api.rs
+++ src/model/api.rs
@@ -6,3 +6,10 @@ pub struct ErrorDto {
     /// The error message
     pub error: String,
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct UserDto {
+    pub user_id: i32,
+    pub character_id: i64,
+    pub character_name: String,
+}
--- src/server/service/auth/callback.rs
+++ src/server/service/auth/callback.rs
@@ -1,8 +1,12 @@
 use oauth2::TokenResponse;
 use sea_orm::DatabaseConnection;
 
-use crate::server::{
-    error::Error, model::auth::Character, service::eve::character::CharacterService,
+use crate::{
+    model::api::UserDto,
+    server::{
+        error::Error,
+        service::{eve::character::CharacterService, user::UserService},
+    },
 };
 
 /// Callback service which fetches & validates JWT token after successful login
@@ -10,20 +14,27 @@ use crate::server::{
 /// Uses an authorization code to fetch a JWT token which provides the user's character name,
 /// ID, as well as an access & refresh token used for fetching data related to the requested scopes.
 ///
+/// - A character will then be either found in the database or created after fetching character information
+///   from ESI using the character's ID found in the token.
+/// - The database will then be checked to see if any user owns the character, if not a new user will be
+///   created and linked to the character.
+///
 /// # Arguments
+/// - `db`: (&[`DatabaseConnection`]): Connection to access database
 /// - `esi_client` ([`eve_esi::Client`]): ESI Client used to fetch and validate JWT token
 /// - `code` (`&str`): Authorization code used to fetch JWT token
 ///
 /// # Returns
 /// Returns a result containing either:
-/// - [`Character`]: Character name & ID for the user
+/// - [`UserDto`]: User's ID, character ID, & character name
 /// - [`Error`]: An error if JWT token fetching or validation fails
 pub async fn callback_service(
     db: &DatabaseConnection,
     esi_client: &eve_esi::Client,
     code: &str,
-) -> Result<Character, Error> {
+) -> Result<UserDto, Error> {
     let character_service = CharacterService::new(&db, &esi_client);
+    let user_service = UserService::new(&db);
 
     let token = esi_client.oauth2().get_token(code).await?;
 
@@ -34,16 +45,28 @@ pub async fn callback_service(
 
     let character_id = claims.character_id()?;
 
+    // TODO: If character needs to be created, we can skip getting user character
+    // ownership to find a user and just simply create the new user and create
+    // the ownership entry.
+    //
+    // This change would save 1 database call
+    //
+    // This is viable because the user ownership entry is linked to character via
+    // a foreign key, it is not possible to have an ownership entry if the character
+    // does not exist in the database.
     let character = character_service
         .get_or_create_character(character_id)
         .await?;
 
-    let character = Character {
+    let user_id = user_service.get_or_create_user(character.id).await?;
+
+    let user = UserDto {
+        user_id: user_id,
         character_id: character.character_id,
         character_name: character.name,
     };
 
-    Ok(character)
+    Ok(user)
 }
 
 #[cfg(test)]
@@ -63,15 +86,16 @@ mod tests {
 
     async fn setup() -> Result<TestSetup, DbErr> {
         let test = test_setup().await;
-
         let db = &test.state.db;
-        let schema = Schema::new(DbBackend::Sqlite);
 
+        let schema = Schema::new(DbBackend::Sqlite);
         let stmts = vec![
             schema.create_table_from_entity(entity::prelude::EveFaction),
             schema.create_table_from_entity(entity::prelude::EveAlliance),
             schema.create_table_from_entity(entity::prelude::EveCorporation),
             schema.create_table_from_entity(entity::prelude::EveCharacter),
+            schema.create_table_from_entity(entity::prelude::BifrostUser),
+            schema.create_table_from_entity(entity::prelude::BifrostUserCharacter),
         ];
 
         for stmt in stmts {
