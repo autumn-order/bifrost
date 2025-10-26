6091d3e9ac05cc6710731a7e7b8f59f6dbed7953
Hyziri <hyziri@protonmail.com>
2025-10-26 04:10:39 -0500

    feat: add link_character method to UserService

--- src/server/service/user.rs
+++ src/server/service/user.rs
@@ -45,6 +45,68 @@ impl<'a> UserService<'a> {
 
         Ok(new_user.id)
     }
+
+    /// Links or transfers character to provided user ID
+    ///
+    /// # Behavior
+    /// - If the character is already linked to the provided user (owner hash matches `claims.owner`),
+    ///   no action is taken and the method returns `Ok(false)`.
+    /// - If the character is linked to a different owner hash, the method currently returns `Ok(true)`
+    ///   to indicate a transfer to the provided user ID
+    /// - If the character exists but has no owner, a link is created that associates the
+    ///   character with the provided `user_id` and owner hash, and the method returns `Ok(true)`.
+    /// - If the character does not exist, it is fetched/created via ESI and then linked to `user_id`,
+    ///   and the method returns `Ok(true)`
+    ///
+    /// # Arguments
+    /// - `user_id` (`i32`): The ID of the user to link or transfer the character to. If the user does not exist
+    ///   in the database then a database error will be returned (foreign-key constraint)
+    /// - `claims` ([`EveJwtClaims`]): JWT claims returned by EVE OAuth2. Contains the character ID
+    ///   (`claims.character_id()`) and an owner hash (`claims.owner`) used to determine current ownership.
+    ///
+    /// # Returns
+    /// - `Ok(false)`: No link was created because the character is already linked to the `claims.owner`
+    /// - `Ok(true)`: A link was created or the character was transferred to the provided user ID
+    /// - `Err(Error::DbErr(_))`: Database error such as a foreign key violation due to invalid `user_id`
+    /// - `Err(Error::EsiError(_))`: Error when making an ESI request for character information or parsing
+    ///   character ID from claims (e.g. `claims.character_id()`)
+    pub async fn link_character(&self, user_id: i32, claims: EveJwtClaims) -> Result<bool, Error> {
+        let user_character_repo = UserCharacterRepository::new(&self.db);
+        let character_service = CharacterService::new(&self.db, &self.esi_client);
+
+        let character_id = claims.character_id()?;
+
+        // If the character exists, check ownership
+        if let Some((character, maybe_owner)) = user_character_repo
+            .get_by_character_id(character_id)
+            .await?
+        {
+            if let Some(owner) = maybe_owner {
+                if owner.owner_hash == claims.owner {
+                    // already linked to this owner -> nothing to do
+                    return Ok(false);
+                }
+
+                // TODO: existing character linked to different owner -> transfer
+                return Ok(true);
+            }
+
+            // existing character but no owner -> create link
+            user_character_repo
+                .create(user_id, character.id, claims.owner)
+                .await?;
+
+            return Ok(true);
+        }
+
+        // character doesn't exist -> create, then link
+        let character = character_service.create_character(character_id).await?;
+        user_character_repo
+            .create(user_id, character.id, claims.owner)
+            .await?;
+
+        Ok(true)
+    }
 }
 
 #[cfg(test)]
@@ -53,7 +115,7 @@ mod tests {
 
     use crate::server::util::test::setup::{test_setup, TestSetup};
 
-    async fn setup() -> Result<TestSetup, DbErr> {
+    async fn test_setup_module() -> Result<TestSetup, DbErr> {
         let test = test_setup().await;
         let db = &test.state.db;
 
@@ -78,50 +140,22 @@ mod tests {
         use eve_esi::model::oauth2::EveJwtClaims;
 
         use crate::server::{
-            data::{
-                eve::{character::CharacterRepository, corporation::CorporationRepository},
-                user::{user::UserRepository, user_character::UserCharacterRepository},
-            },
             error::Error,
-            service::user::{tests::setup, UserService},
-            util::test::{
-                eve::mock::{mock_character, mock_corporation},
-                mockito::{
-                    character::mock_character_endpoint, corporation::mock_corporation_endpoint,
-                },
-                setup::test_setup,
+            service::user::{tests::test_setup_module, UserService},
+            util::test::setup::{
+                test_setup, test_setup_create_character, test_setup_create_character_endpoints,
+                test_setup_create_user_with_character,
             },
         };
 
         /// Expect success when user associated with character is found
         #[tokio::test]
         async fn test_get_or_create_user_found_user() -> Result<(), Error> {
-            let test = setup().await?;
+            let test = test_setup_module().await?;
+            let character = test_setup_create_character(&test).await?;
+            let _ = test_setup_create_user_with_character(&test, character).await?;
 
-            let character_repo = CharacterRepository::new(&test.state.db);
-            let corporation_repo = CorporationRepository::new(&test.state.db);
-            let user_repo = UserRepository::new(&test.state.db);
-            let user_character_repo = UserCharacterRepository::new(&test.state.db);
             let user_service = UserService::new(&test.state.db, &test.state.esi_client);
-
-            let faction_id = None;
-            let alliance_id = None;
-            let corporation_id = 1;
-            let mock_corporation = mock_corporation(alliance_id, faction_id);
-
-            let character_id = 1;
-            let mock_character = mock_character(corporation_id, alliance_id, faction_id);
-
-            let corporation = corporation_repo
-                .create(corporation_id, mock_corporation, None, None)
-                .await?;
-            let character = character_repo
-                .create(character_id, mock_character, corporation.id, None)
-                .await?;
-            let user = user_repo.create().await?;
-            let _ = user_character_repo
-                .create(user.id, character.id, "owner hash".to_string())
-                .await?;
 
             // Set character ID in claims to the mock character
             let mut claims = EveJwtClaims::mock();
@@ -137,26 +171,10 @@ mod tests {
         /// Expect success when character is found but new user is created
         #[tokio::test]
         async fn test_get_or_create_user_new_user() -> Result<(), Error> {
-            let test = setup().await?;
+            let test = test_setup_module().await?;
+            let _ = test_setup_create_character(&test).await?;
 
-            let character_repo = CharacterRepository::new(&test.state.db);
-            let corporation_repo = CorporationRepository::new(&test.state.db);
             let user_service = UserService::new(&test.state.db, &test.state.esi_client);
-
-            let faction_id = None;
-            let alliance_id = None;
-            let corporation_id = 1;
-            let mock_corporation = mock_corporation(alliance_id, faction_id);
-
-            let character_id = 1;
-            let mock_character = mock_character(corporation_id, alliance_id, faction_id);
-
-            let corporation = corporation_repo
-                .create(corporation_id, mock_corporation, None, None)
-                .await?;
-            let _ = character_repo
-                .create(character_id, mock_character, corporation.id, None)
-                .await?;
 
             // Set character ID in claims to the mock character
             let mut claims = EveJwtClaims::mock();
@@ -172,21 +190,11 @@ mod tests {
         /// Expect success when new character & user is created
         #[tokio::test]
         async fn test_get_or_create_user_new_character() -> Result<(), Error> {
-            let mut test = setup().await?;
+            let mut test = test_setup_module().await?;
+            let (mock_character_endpoint, mock_corporation_endpoint) =
+                test_setup_create_character_endpoints(&mut test).await;
 
             let user_service = UserService::new(&test.state.db, &test.state.esi_client);
-
-            let faction_id = None;
-            let alliance_id = None;
-            let corporation_id = 1;
-            let mock_corporation = mock_corporation(alliance_id, faction_id);
-
-            let mock_character = mock_character(corporation_id, alliance_id, faction_id);
-
-            let mock_corporation_endpoint =
-                mock_corporation_endpoint(&mut test.server, "/corporations/1", mock_corporation, 1);
-            let mock_character_endpoint =
-                mock_character_endpoint(&mut test.server, "/characters/1", mock_character, 1);
 
             // Set character ID in claims to the mock character
             let mut claims = EveJwtClaims::mock();
@@ -226,7 +234,7 @@ mod tests {
         /// Expect Error when required ESI endpoints are unavailable
         #[tokio::test]
         async fn test_get_or_create_user_esi_error() -> Result<(), Error> {
-            let test = setup().await?;
+            let test = test_setup_module().await?;
 
             let user_service = UserService::new(&test.state.db, &test.state.esi_client);
 
@@ -237,6 +245,163 @@ mod tests {
             claims.sub = "CHARACTER:EVE:1".to_string();
 
             let result = user_service.get_or_create_user(claims).await;
+
+            assert!(result.is_err());
+            assert!(matches!(result, Err(Error::EsiError(_))));
+
+            Ok(())
+        }
+    }
+
+    mod link_character_tests {
+        use eve_esi::model::oauth2::EveJwtClaims;
+
+        use crate::server::{
+            data::user::user::UserRepository,
+            error::Error,
+            service::user::{tests::test_setup_module, UserService},
+            util::test::setup::{
+                test_setup_create_character, test_setup_create_character_endpoints,
+                test_setup_create_user_with_character,
+            },
+        };
+
+        /// Expect no link created when finding character owned by provided user ID
+        #[tokio::test]
+        async fn test_link_character_owned_success() -> Result<(), Error> {
+            let test = test_setup_module().await?;
+            let character = test_setup_create_character(&test).await?;
+            let character_ownership =
+                test_setup_create_user_with_character(&test, character).await?;
+
+            let user_service = UserService::new(&test.state.db, &test.state.esi_client);
+
+            let mut claims = EveJwtClaims::mock();
+            claims.sub = "CHARACTER:EVE:1".to_string();
+            claims.owner = "test_owner_hash".to_string();
+
+            let result = user_service
+                .link_character(character_ownership.user_id, claims)
+                .await;
+
+            assert!(result.is_ok());
+            let link_created = result.unwrap();
+
+            assert!(!link_created);
+
+            Ok(())
+        }
+
+        /// Expect link created when an already owned character is transferred to provided user ID
+        #[tokio::test]
+        async fn test_link_character_owned_transfer_success() -> Result<(), Error> {
+            let test = test_setup_module().await?;
+            let character = test_setup_create_character(&test).await?;
+            let _ = test_setup_create_user_with_character(&test, character).await?;
+
+            let user_repo = UserRepository::new(&test.state.db);
+            let user_service = UserService::new(&test.state.db, &test.state.esi_client);
+
+            let mut claims = EveJwtClaims::mock();
+            claims.sub = "CHARACTER:EVE:1".to_string();
+
+            let user = user_repo.create().await?;
+            let result = user_service.link_character(user.id, claims).await;
+
+            assert!(result.is_ok());
+            let link_created = result.unwrap();
+
+            assert!(link_created);
+
+            // TODO: assert character has actually been transferred to the new user ID
+            // - Will be done when transfer method is implemented
+
+            Ok(())
+        }
+
+        /// Expect link created when character is created but not owned and linked to provided user ID
+        #[tokio::test]
+        async fn test_link_character_not_owned_success() -> Result<(), Error> {
+            let test = test_setup_module().await?;
+            let _ = test_setup_create_character(&test).await?;
+
+            let user_repo = UserRepository::new(&test.state.db);
+            let user_service = UserService::new(&test.state.db, &test.state.esi_client);
+
+            let mut claims = EveJwtClaims::mock();
+            claims.sub = "CHARACTER:EVE:1".to_string();
+
+            let user = user_repo.create().await?;
+            let result = user_service.link_character(user.id, claims).await;
+
+            assert!(result.is_ok());
+            let link_created = result.unwrap();
+
+            assert!(link_created);
+
+            Ok(())
+        }
+
+        /// Expect link created when creating a new character and linking to provided user ID
+        #[tokio::test]
+        async fn test_link_character_create_character_success() -> Result<(), Error> {
+            let mut test = test_setup_module().await?;
+            let (mock_character_endpoint, mock_corporation_endpoint) =
+                test_setup_create_character_endpoints(&mut test).await;
+
+            let user_repo = UserRepository::new(&test.state.db);
+            let user_service = UserService::new(&test.state.db, &test.state.esi_client);
+
+            let mut claims = EveJwtClaims::mock();
+            claims.sub = "CHARACTER:EVE:1".to_string();
+
+            let user = user_repo.create().await?;
+            let result = user_service.link_character(user.id, claims).await;
+
+            assert!(result.is_ok());
+            let link_created = result.unwrap();
+
+            assert!(link_created);
+
+            // Assert 1 request was made to each mock endpoint
+            mock_corporation_endpoint.assert();
+            mock_character_endpoint.assert();
+
+            Ok(())
+        }
+
+        /// Expect database Error when user ID provided does not exist in database
+        #[tokio::test]
+        async fn test_link_character_user_id_foreign_key_database_error() -> Result<(), Error> {
+            let test = test_setup_module().await?;
+            let _ = test_setup_create_character(&test).await?;
+
+            let user_service = UserService::new(&test.state.db, &test.state.esi_client);
+
+            let mut claims = EveJwtClaims::mock();
+            claims.sub = "CHARACTER:EVE:1".to_string();
+
+            let user_id = 1;
+            let result = user_service.link_character(user_id, claims).await;
+
+            assert!(result.is_err());
+            assert!(matches!(result, Err(Error::DbErr(_))));
+
+            Ok(())
+        }
+
+        /// Expect ESI error when endpoints required to create a character are not available
+        #[tokio::test]
+        async fn test_link_character_create_character_esi_error() -> Result<(), Error> {
+            let test = test_setup_module().await?;
+
+            let user_service = UserService::new(&test.state.db, &test.state.esi_client);
+
+            let mut claims = EveJwtClaims::mock();
+            claims.sub = "CHARACTER:EVE:1".to_string();
+
+            let user_id = 1;
+            let result = user_service.link_character(user_id, claims).await;
 
             assert!(result.is_err());
             assert!(matches!(result, Err(Error::EsiError(_))));
--- src/server/util/test/setup.rs
+++ src/server/util/test/setup.rs
@@ -1,10 +1,21 @@
 use std::sync::Arc;
 
-use mockito::{Server, ServerGuard};
+use mockito::{Mock, Server, ServerGuard};
 use sea_orm::Database;
 use tower_sessions::{MemoryStore, Session};
 
-use crate::server::model::app::AppState;
+use crate::server::{
+    data::{
+        eve::{character::CharacterRepository, corporation::CorporationRepository},
+        user::{user::UserRepository, user_character::UserCharacterRepository},
+    },
+    error::Error,
+    model::app::AppState,
+    util::test::{
+        eve::mock::{mock_character, mock_corporation},
+        mockito::{character::mock_character_endpoint, corporation::mock_corporation_endpoint},
+    },
+};
 
 pub static TEST_USER_AGENT: &str =
     "MyApp/1.0 (contact@example.com; +https://github.com/autumn-order/bifrost)";
@@ -54,4 +65,62 @@ pub async fn test_setup() -> TestSetup {
         state,
         session,
     }
+}
+
+/// Inserts mock data for an EVE Online character
+pub async fn test_setup_create_character(
+    test: &TestSetup,
+) -> Result<entity::eve_character::Model, Error> {
+    let character_repo = CharacterRepository::new(&test.state.db);
+    let corporation_repo = CorporationRepository::new(&test.state.db);
+
+    let faction_id = None;
+    let alliance_id = None;
+    let corporation_id = 1;
+    let mock_corporation = mock_corporation(alliance_id, faction_id);
+
+    let character_id = 1;
+    let mock_character = mock_character(corporation_id, alliance_id, faction_id);
+
+    let corporation = corporation_repo
+        .create(corporation_id, mock_corporation, None, None)
+        .await?;
+    let character = character_repo
+        .create(character_id, mock_character, corporation.id, None)
+        .await?;
+
+    Ok(character)
+}
+
+/// Inserts mock data for a user which owns a character
+pub async fn test_setup_create_user_with_character(
+    test: &TestSetup,
+    character: entity::eve_character::Model,
+) -> Result<entity::bifrost_user_character::Model, Error> {
+    let user_repo = UserRepository::new(&test.state.db);
+    let user_character_repo = UserCharacterRepository::new(&test.state.db);
+
+    let user = user_repo.create().await?;
+    let character_ownership = user_character_repo
+        .create(user.id, character.id, "test_owner_hash".to_string())
+        .await?;
+
+    Ok(character_ownership)
+}
+
+/// Provides mock ESI endpoints required for creating a new character
+pub async fn test_setup_create_character_endpoints(test: &mut TestSetup) -> (Mock, Mock) {
+    let faction_id = None;
+    let alliance_id = None;
+    let corporation_id = 1;
+    let mock_corporation = mock_corporation(alliance_id, faction_id);
+
+    let mock_character = mock_character(corporation_id, alliance_id, faction_id);
+
+    let mock_corporation_endpoint =
+        mock_corporation_endpoint(&mut test.server, "/corporations/1", mock_corporation, 1);
+    let mock_character_endpoint =
+        mock_character_endpoint(&mut test.server, "/characters/1", mock_character, 1);
+
+    (mock_character_endpoint, mock_corporation_endpoint)
 }
