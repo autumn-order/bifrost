2e0d7843c4d801d259b0bcd9e16bde6bef5dc73e
Hyziri <hyziri@protonmail.com>
2025-10-29 00:57:28 -0500

    tests: implement shared test utils crate for corporation service

--- bifrost-test-utils/src/eve/mockito.rs
+++ bifrost-test-utils/src/eve/mockito.rs
@@ -32,4 +32,23 @@ impl TestSetup {
             .expect(expected_requests)
             .create()
     }
+
+    pub fn with_corporation_endpoint(
+        &mut self,
+        corporation_id: i64,
+        alliance_id: Option<i64>,
+        faction_id: Option<i64>,
+        expected_requests: usize,
+    ) -> Mock {
+        let (_, corporation) = self.with_mock_corporation(corporation_id, alliance_id, faction_id);
+        let url = format!("/corporations/{}", corporation_id);
+
+        self.server
+            .mock("GET", url.as_str())
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(serde_json::to_string(&corporation).unwrap())
+            .expect(expected_requests)
+            .create()
+    }
 }
