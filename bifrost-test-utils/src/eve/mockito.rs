4848ac699c4e88e76b0d1d189d3841e73384f904
Hyziri <hyziri@protonmail.com>
2025-10-29 00:01:12 -0500

    refactor: implement shared test utils crate for alliance service

--- bifrost-test-utils/src/eve/mockito.rs
+++ bifrost-test-utils/src/eve/mockito.rs
@@ -14,4 +14,22 @@ impl TestSetup {
             .expect(expected_requests)
             .create()
     }
+
+    pub fn with_alliance_endpoint(
+        &mut self,
+        alliance_id: i64,
+        faction_id: Option<i64>,
+        expected_requests: usize,
+    ) -> Mock {
+        let (_, alliance) = self.with_mock_alliance(alliance_id, faction_id);
+        let url = format!("/alliances/{}", alliance_id);
+
+        self.server
+            .mock("GET", url.as_str())
+            .with_status(200)
+            .with_header("content-type", "application/json")
+            .with_body(serde_json::to_string(&alliance).unwrap())
+            .expect(expected_requests)
+            .create()
+    }
 }
--- bifrost-test-utils/src/lib.rs
+++ bifrost-test-utils/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod constant;
 pub mod error;
 pub mod eve;
+pub mod prelude;
 pub mod setup;
 pub mod user;
 
