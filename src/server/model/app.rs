f6dd25b56d2b0b9795ecf4676ea1daca8a29920d
Hyziri <hyziri@protonmail.com>
2025-10-31 16:34:28 -0500

    tests: implement shared tests crate for login controller tests

--- src/server/model/app.rs
+++ src/server/model/app.rs
@@ -5,3 +5,10 @@ pub struct AppState {
     pub db: DatabaseConnection,
     pub esi_client: eve_esi::Client,
 }
+
+// Used for `bifrost_test_utils` impl for the `state()` method to convert TestAppState to AppState
+impl From<(DatabaseConnection, eve_esi::Client)> for AppState {
+    fn from((db, esi_client): (DatabaseConnection, eve_esi::Client)) -> Self {
+        AppState { db, esi_client }
+    }
+}
