4101ad721b529022b1e989087645e54dd7d616d2
Hyziri <hyziri@protonmail.com>
2025-11-05 22:52:10 -0600

    feat: implement upsert_many method for EVE alliance repository

--- src/server/data/eve/alliance.rs
+++ src/server/data/eve/alliance.rs
@@ -80,6 +80,50 @@ impl<'a> AllianceRepository<'a> {
         )
     }
 
+    /// Create or update many based upon provided alliance ID, ESI alliance model, and optional faction table entry ID
+    pub async fn upsert_many(
+        &self,
+        alliances: Vec<(i64, Alliance, Option<i32>)>,
+    ) -> Result<Vec<entity::eve_alliance::Model>, DbErr> {
+        let alliances = alliances
+            .into_iter()
+            .map(
+                |(alliance_id, alliance, faction_id)| entity::eve_alliance::ActiveModel {
+                    alliance_id: ActiveValue::Set(alliance_id),
+                    faction_id: ActiveValue::Set(faction_id),
+                    creator_corporation_id: ActiveValue::Set(alliance.creator_corporation_id),
+                    executor_corporation_id: ActiveValue::Set(alliance.executor_corporation_id),
+                    creator_id: ActiveValue::Set(alliance.creator_id),
+                    date_founded: ActiveValue::Set(alliance.date_founded.naive_utc()),
+                    name: ActiveValue::Set(alliance.name),
+                    ticker: ActiveValue::Set(alliance.ticker),
+                    created_at: ActiveValue::Set(Utc::now().naive_utc()),
+                    updated_at: ActiveValue::Set(Utc::now().naive_utc()),
+                    job_scheduled_at: ActiveValue::Set(None),
+                    ..Default::default()
+                },
+            );
+
+        entity::prelude::EveAlliance::insert_many(alliances)
+            .on_conflict(
+                OnConflict::column(entity::eve_alliance::Column::AllianceId)
+                    .update_columns([
+                        entity::eve_alliance::Column::FactionId,
+                        entity::eve_alliance::Column::CreatorCorporationId,
+                        entity::eve_alliance::Column::ExecutorCorporationId,
+                        entity::eve_alliance::Column::CreatorId,
+                        entity::eve_alliance::Column::DateFounded,
+                        entity::eve_alliance::Column::Name,
+                        entity::eve_alliance::Column::Ticker,
+                        entity::eve_alliance::Column::UpdatedAt,
+                        entity::eve_alliance::Column::JobScheduledAt,
+                    ])
+                    .to_owned(),
+            )
+            .exec_with_returning(self.db)
+            .await
+    }
+
     /// Get an alliance using its EVE Online alliance ID
     pub async fn get_by_alliance_id(
         &self,
@@ -318,6 +362,90 @@ mod tests {
             let result = alliance_repo.upsert(alliance_id, alliance, None).await;
 
             assert!(result.is_err());
+
+            Ok(())
+        }
+    }
+
+    mod upsert_many {
+        use super::*;
+
+        /// Expect Ok when upserting new alliances
+        #[tokio::test]
+        async fn upserts_new_alliances() -> Result<(), TestError> {
+            let mut test =
+                test_setup_with_tables!(entity::prelude::EveFaction, entity::prelude::EveAlliance)?;
+            let (alliance_id_1, alliance_1) = test.eve().with_mock_alliance(1, None);
+            let (alliance_id_2, alliance_2) = test.eve().with_mock_alliance(2, None);
+
+            let alliance_repo = AllianceRepository::new(&test.state.db);
+            let result = alliance_repo
+                .upsert_many(vec![
+                    (alliance_id_1, alliance_1, None),
+                    (alliance_id_2, alliance_2, None),
+                ])
+                .await;
+
+            assert!(result.is_ok(), "Error: {:?}", result);
+            let created_alliances = result.unwrap();
+            assert_eq!(created_alliances.len(), 2);
+
+            Ok(())
+        }
+
+        /// Expect Ok & update when trying to upsert existing alliances
+        #[tokio::test]
+        async fn updates_existing_alliances() -> Result<(), TestError> {
+            let mut test =
+                test_setup_with_tables!(entity::prelude::EveFaction, entity::prelude::EveAlliance)?;
+            let (alliance_id_1, alliance_1) = test.eve().with_mock_alliance(1, None);
+            let (alliance_id_2, alliance_2) = test.eve().with_mock_alliance(2, None);
+            let (alliance_id_1_update, alliance_1_update) = test.eve().with_mock_alliance(1, None);
+            let (alliance_id_2_update, alliance_2_update) = test.eve().with_mock_alliance(2, None);
+
+            let alliance_repo = AllianceRepository::new(&test.state.db);
+            let initial = alliance_repo
+                .upsert_many(vec![
+                    (alliance_id_1, alliance_1, None),
+                    (alliance_id_2, alliance_2, None),
+                ])
+                .await?;
+
+            let initial_entry_1 = initial
+                .iter()
+                .find(|a| a.alliance_id == alliance_id_1)
+                .expect("alliance 1 not found");
+            let initial_entry_2 = initial
+                .iter()
+                .find(|a| a.alliance_id == alliance_id_2)
+                .expect("alliance 2 not found");
+
+            let initial_created_at_1 = initial_entry_1.created_at;
+            let initial_updated_at_1 = initial_entry_1.updated_at;
+            let initial_created_at_2 = initial_entry_2.created_at;
+            let initial_updated_at_2 = initial_entry_2.updated_at;
+
+            let latest = alliance_repo
+                .upsert_many(vec![
+                    (alliance_id_1_update, alliance_1_update, None),
+                    (alliance_id_2_update, alliance_2_update, None),
+                ])
+                .await?;
+
+            let latest_entry_1 = latest
+                .iter()
+                .find(|a| a.alliance_id == alliance_id_1_update)
+                .expect("alliance 1 not found");
+            let latest_entry_2 = latest
+                .iter()
+                .find(|a| a.alliance_id == alliance_id_2_update)
+                .expect("alliance 2 not found");
+
+            // created_at should not change and updated_at should increase for both alliances
+            assert_eq!(latest_entry_1.created_at, initial_created_at_1);
+            assert!(latest_entry_1.updated_at > initial_updated_at_1);
+            assert_eq!(latest_entry_2.created_at, initial_created_at_2);
+            assert!(latest_entry_2.updated_at > initial_updated_at_2);
 
             Ok(())
         }
