be559aa2ef2c0be8eb8d2060d4a20442e11e2780
Hyziri <hyziri@protonmail.com>
2025-11-11 19:54:41 -0600

    refactor: use new get_user_characters service method for get_user_characters controller

--- src/server/controller/user.rs
+++ src/server/controller/user.rs
@@ -1,25 +1,18 @@
 use axum::{extract::State, http::StatusCode, response::IntoResponse};
 use dioxus_logger::tracing;
-use sea_orm::{ColumnTrait, EntityTrait, QueryFilter};
 use tower_sessions::Session;
 
 use crate::{
-    model::{
-        api::ErrorDto,
-        user::{AllianceDto, CharacterDto, CorporationDto},
-    },
+    model::{api::ErrorDto, user::CharacterDto},
     server::{
         error::Error,
         model::{app::AppState, session::user::SessionUserId},
-        service::user::UserService,
+        service::user::{user_character::UserCharacterService, UserService},
     },
 };
 
 pub static USER_TAG: &str = "user";
 
-// TEMPORARY for alpha 1 tests
-//
-// Refactor will be needed to decouple sea_orm and remove updated at metadata out of this controller for actual usage outside of tests
 /// Get all characters owned by logged in user
 #[utoipa::path(
     get,
@@ -72,78 +65,9 @@ pub async fn get_user_characters(
             .into_response());
     };
 
-    let user_characters_join = entity::prelude::BifrostUserCharacter::find()
-        .filter(entity::bifrost_user_character::Column::UserId.eq(user.id))
-        .find_also_related(entity::prelude::EveCharacter)
-        .all(&state.db)
+    let character_dtos = UserCharacterService::new(&state.db, &state.esi_client)
+        .get_user_characters(user.id)
         .await?;
-
-    let user_characters: Vec<entity::eve_character::Model> = user_characters_join
-        .into_iter()
-        .filter_map(|(_, maybe_char)| maybe_char)
-        .collect();
-
-    let corporation_ids = user_characters
-        .iter()
-        .map(|c| c.corporation_id)
-        .collect::<Vec<i32>>();
-
-    // Get related corporations
-    let corporations = entity::eve_corporation::Entity::find()
-        .filter(entity::eve_corporation::Column::Id.is_in(corporation_ids))
-        .all(&state.db)
-        .await?;
-
-    let alliance_ids = corporations
-        .iter()
-        .filter_map(|c| c.alliance_id)
-        .collect::<Vec<i32>>();
-
-    let alliances = entity::eve_alliance::Entity::find()
-        .filter(entity::eve_alliance::Column::Id.is_in(alliance_ids))
-        .all(&state.db)
-        .await?;
-
-    let character_dtos: Vec<CharacterDto> = user_characters
-        .into_iter()
-        .filter_map(|character| {
-            let corporation = corporations
-                .iter()
-                .find(|corp| corp.id == character.corporation_id);
-
-            if let Some(corporation) = corporation {
-                let alliance = corporation.alliance_id.and_then(|alliance_id| {
-                    alliances.iter().find(|alliance| alliance.id == alliance_id)
-                });
-
-                let alliance_dto = if let Some(alliance) = alliance {
-                    Some(AllianceDto {
-                        id: alliance.alliance_id,
-                        name: alliance.name.clone(),
-                        updated_at: alliance.updated_at,
-                    })
-                } else {
-                    None
-                };
-
-                Some(CharacterDto {
-                    id: character.character_id,
-                    name: character.name,
-                    corporation: CorporationDto {
-                        id: corporation.corporation_id,
-                        name: corporation.name.clone(),
-                        info_updated_at: corporation.info_updated_at,
-                        affiliation_updated_at: corporation.affiliation_updated_at,
-                    },
-                    alliance: alliance_dto,
-                    info_updated_at: character.info_updated_at,
-                    affiliation_updated_at: character.affiliation_updated_at,
-                })
-            } else {
-                None
-            }
-        })
-        .collect();
 
     Ok((StatusCode::OK, axum::Json(character_dtos)).into_response())
 }
