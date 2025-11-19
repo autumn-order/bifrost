dd6e0414c007c0a86bb4d866e94f72af77d014cb
Hyziri <hyziri@protonmail.com>
2025-11-18 23:47:33 -0600

    refactor: attempt to get factions from cache first before starting fetch process

--- src/server/service/eve/faction.rs
+++ src/server/service/eve/faction.rs
@@ -109,28 +109,27 @@ pub(super) async fn fetch_factions(
     esi_client: &eve_esi::Client,
     retry_cache: &mut Option<Vec<Faction>>,
 ) -> Result<Option<(Vec<Faction>, bool)>, Error> {
+    // Try to get factions from cache
+    if let Some(cached) = retry_cache.as_ref() {
+        let cached_factions = cached.clone();
+
+        return Ok(Some((cached_factions, true)));
+    }
+
     let faction_repo = FactionRepository::new(db);
 
     let now = Utc::now();
     let effective_expiry = effective_faction_cache_expiry(now)?;
 
     // If the latest faction entry was updated at or after the effective expiry, skip updating.
-    if let Some(faction) = faction_repo.get_latest().await? {
-        if faction.updated_at >= effective_expiry {
+    if let Some(latest_faction_model) = faction_repo.get_latest().await? {
+        if latest_faction_model.updated_at >= effective_expiry {
             return Ok(None);
         }
     }
 
-    // Get factions from cache or fetch from ESI
-    if let Some(cached) = retry_cache.as_ref() {
-        // Use cached factions
-        let factions = cached.clone();
-
-        Ok(Some((factions, true)))
-    } else {
-        // First attempt: fetch from ESI and cache the result
-        let factions = esi_client.universe().get_factions().await?;
-        *retry_cache = Some(factions.clone());
-        Ok(Some((factions, false)))
-    }
+    // First attempt: fetch from ESI and cache the result
+    let fetched_factions = esi_client.universe().get_factions().await?;
+    *retry_cache = Some(fetched_factions.clone());
+    Ok(Some((fetched_factions, false)))
 }
